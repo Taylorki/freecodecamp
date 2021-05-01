c7588be5d55846facd4dba823b056f2b7ce68ae6
SebastiaanSimons <Sebastiaansimons@outlook.com>
2021-05-01 21:31:24 +0200

    add console sidebar

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
 function showIam() {
-  console.log("hello");
   document.getElementById("iam").style.visibility = "visible";
 }
 
-function hideIam() {
-  console.log("go away");
-  document.getElementById("iam").style.visibility = "hidden";
-}
+function showOld() {
+    document.getElementById("old").style.visibility = "visible";
+  }
 
 function openNav() {
-  document.getElementById("mySidebar").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
+  document.getElementById("mySidebar").style.width = "25vw";
+  document.getElementById("main").style.marginLeft = "25vw";
+  document.getElementById("open-console").style.visibility = "hidden";
+  document.getElementById("log-1").style.visibility = "visible";
+  setTimeout(function () {
+    document.getElementById("log-2").style.visibility = "visible";
+  }, 2000);
+  setTimeout(function () {
+    document.getElementById("log-3").style.visibility = "visible";
+  }, 4000);
+  setTimeout(function () {
+    document.getElementById("log-4").style.visibility = "visible";
+  }, 6000);
 }
 
 function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
   document.getElementById("main").style.marginLeft = "0";
+  document.getElementById("open-console").style.visibility = "visible";
 }
+
+openNav();
