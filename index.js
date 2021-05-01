a9079f611467d384f3b88a6bdd78bc0dd45d607a
SebastiaanSimons <Sebastiaansimons@outlook.com>
2021-05-01 23:05:58 +0200

    improvements on the console bar / flippin' images

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const logs = {
+  0: "> Console cleared!",
   1: "> Initiating...",
   2: "> Gathering data from subject with id #SEBA-25-6-6-93...",
   3: "> Checking subject's Facebook activity log...",
   4: "> Done",
   5: '> subjectAlive ? "I am" : "I was"',
   6: '> subjectHappy ? "young" : "old"',
+  7: "> Image flipped!",
 };
 
-const logIdsInUse = [];
+let logIdsInUse = [];
 
 function showLog(id) {
   if (!logIdsInUse.includes(id)) {
@@ -16,14 +18,36 @@ function showLog(id) {
     node.appendChild(textNode);
     document.getElementById("log-files").appendChild(node);
     logIdsInUse.push(id);
+    logClearing(logIdsInUse.length);
+  } else if (id === 7) {
+    let node = document.createElement("P");
+    let textNode = document.createTextNode("> Image flipped again!");
+    node.appendChild(textNode);
+    document.getElementById("log-files").appendChild(node);
+    logIdsInUse.push(id);
+    logClearing(logIdsInUse.length);
   }
+}
+
+function logClearing(numberOfLogs) {
+  if (numberOfLogs === 25) {
+    document.getElementById("log-files").innerHTML = "";
+    logIdsInUse = [];
+    showLog(0);
+  }
+}
+
+function clearLog() {
+  document.getElementById("log-files").innerHTML = "";
+  logIdsInUse = [];
+  showLog(0);
 }
 
 function showOld() {
   document.getElementById("old").style.visibility = "visible";
 }
 
-function openNav() {
+function openLog() {
   document.getElementById("mySidebar").style.width = "25vw";
   document.getElementById("main").style.marginLeft = "25vw";
   document.getElementById("open-console").style.visibility = "hidden";
@@ -45,4 +69,15 @@ function closeNav() {
   document.getElementById("open-console").style.visibility = "visible";
 }
 
-openNav();
+let logInput = document.getElementById("logInput");
+logInput.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+   event.preventDefault();
+   if(document.getElementById("logInput").value === 'clear()') {
+       clearLog(); 
+   }
+  }
+});
+
+openLog();
+document.getElementById("logInput").focus(); 
