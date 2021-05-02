7d0b3e6e2f5ea08864c9c837b1a6e1f8427ce3fa
SebastiaanSimons <Sebastiaansimons@outlook.com>
2021-05-02 12:59:15 +0200

    v1.0 faq

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const logs = {
   5: '> subjectAlive ? "I am" : "I was"',
   6: '> subjectHappy ? "young" : "old"',
   7: "> Image flipped!",
+  8: "> Fetching personal data from Cambridge Analytica servers...", 
+  9: "> Checking grades in university database..."
 };
 
 let logIdsInUse = [];
@@ -52,7 +54,7 @@ function openLog() {
   document.getElementById("main").style.marginLeft = "25vw";
   document.getElementById("mySidebar").style.visibility = "visible";
   document.getElementById("open-console").style.visibility = "hidden";
-  /*showLog(1);
+  showLog(1);
   setTimeout(function () {
     showLog(2);
   }, 2000);
@@ -61,7 +63,7 @@ function openLog() {
   }, 4000);
   setTimeout(function () {
     showLog(4);
-  }, 6000);*/
+  }, 6000);
 }
 
 function closeNav() {
@@ -95,7 +97,7 @@ openLog();
 document.getElementById("logInput").focus(); 
 
 let firstAnswer = `<div class='currentAnswer'>
-<p>My name is Sebastiaan (Seb), I'm 27 years old and I'm originally from Antwerp, Belgium.<br/>
+<p>My name is Sebastiaan (Seb), <span onmouseover="showLog(5)">I am </span> 27 years <span onmouseover="showLog(6)">old</span> and I'm originally from Antwerp, Belgium.<br/>
 I'm a software developer at heart and I have a wide range of skills and 
 expertise within this field. <br> 
 Apart from programming, I love traveling, writing fiction and playing the piano. 
@@ -114,15 +116,92 @@ Apart from programming, I love traveling, writing fiction and playing the piano.
 </div>
  `; 
 
+ let thirdAnswer = `
+ <div class='currentAnswer'>
+ 
+ <p> I started as a marketing specialist for a medium-sized catering company. I was part of the rebranding process, <br/>
+ creating a newer and more recognizable brand, which simultaneously stood for the values the company wanted to represent. </p>
+ <p> After that, I started working for a Salesforce implementation partner, first as a consultant, and then as a developer. <br/>
+ And I have been doing exactly that for the past 3-ish years, building a vast array of experience in Salesforce along the way. <br/>
+ I have touched nearly all aspects of Salesforce development on the core platform (Apex, Visualforce, LWC, SOQL), as well as <br/>
+ on its Marketing Cloud, which includes languages such as html/css, handlebars, AMPscript and SQL. 
+ </p>
+ </div>
+ `; 
+
+ let fourthAnswer = `
+ <div class='currentAnswer'>
+ <p> I am responsible for multiple clients mostly situated in the cultural sector. These clients usually have similar requirements <br/>
+ for their Salesforce org:
+ <ul>
+ <li>
+ complex logic including building custom interfaces, trigger logic and asynchronous work 
+ </li>
+ <li>
+ integrations with their ticketing system, website and/or planning application 
+ </li>
+ <li>
+ sending out highly personalized, relevant mails
+ </li>
+ <li>
+ a blog building interface 
+ </li>
+ </ul>
+ And so on... <br/>
+ On a day-to-day basis, I am either building or maintaining these requirements.
+ </p>
+ </div>
+ `; 
+
+ let fifthAnswer = `
+ <div class='currentAnswer'>
+ <ul>
+ <li> Dutch: native </li>
+ <li> English: fluent / near-native </li>
+ <li> French: intermediate </li>
+ <li> German: beginner </li>
+ </ul>
+ </div>
+ `; 
+
+ let sixthAnswer = `
+ <div class='currentAnswer'>
+
+ <p> Back in 2018, I went to Vietnam for 3 months to aid university students with their English study <br/>
+ through a series of discussion classes. It was a wonderful experience, enabling me to empower others and <br/> 
+ at the same time, have a deeper self-understanding.
+ </p>
+
+ </div>
+ `; 
+
+ let seventhAnswer = `
+ <div class='currentAnswer flip-image'>
+  <div class="flip-box">
+    <div class="flip-box-inner">
+      
+      <div class="flip-box-front">
+        <img onmouseover="showLog(7)" width="150" src="profilePicture.png" />
+      </div>
+      
+      <div class="flip-box-back">
+        <p>It's me!</p>
+      </div>
+
+    </div>
+  </div>
+ </div>
+ `; 
+
+
  const answers = {
   0: firstAnswer,
   1: secondAnswer,
-  2: "> Gathering data from subject with id #SEBA-25-6-6-93...",
-  3: "> Checking subject's Facebook activity log...",
-  4: "> Done",
-  5: '> subjectAlive ? "I am" : "I was"',
-  6: '> subjectHappy ? "young" : "old"',
-  7: "> Image flipped!",
+  2: thirdAnswer,
+  3: fourthAnswer,
+  4: fifthAnswer,
+  5: sixthAnswer,
+  6: seventhAnswer,
 };
 
 let currentlyOpen = []; 
