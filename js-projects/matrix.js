3bc79e7b90518e0f0a19adef20c7f1d1ef323b61
Sebastiaan Simons <sebastiaansimons@outlook.com>
2021-10-31 12:59:30 +0100

    matrix project

---
+++ js-projects/matrix.js
@@ -0,0 +1,35 @@
+// 3 rows and 3 columns 
+
+const matrix = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0],
+];
+
+// 4 rows and 2 columns 
+
+const matrix = [
+    [0, 0],
+    [0, 0],
+    [0, 0],
+    [0, 0]
+];
+
+// init = initialize
+// i = index
+
+function init_matrix(number_of_rows, number_of_columns) {
+    let matrix = [];
+    for (let index = 0; index < number_of_rows; index++) {
+        let row = [];
+        for (let j = 0; j < number_of_columns; j++) {
+            row.push(0);
+        }
+        matrix.push(row);
+    }
+    return matrix;
+}
+
+let matrix = init_matrix(3, 3);
+console.log(matrix);
+
